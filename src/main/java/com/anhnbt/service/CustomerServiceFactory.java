37b12ac95bbe6afdf61d18d38d43f75f309488e6
anhnbt-it <anhnbt.it@gmail.com>
2020-12-12 07:16:35 +0700

    first commit

---
+++ src/main/java/com/anhnbt/service/CustomerServiceFactory.java
@@ -0,0 +1,14 @@
+package com.anhnbt.service;
+
+import com.anhnbt.service.impl.SimpleCustomerServiceImpl;
+
+public class CustomerServiceFactory {
+    private static CustomerService singleton;
+
+    public static synchronized CustomerService getInstance() {
+        if (singleton == null) {
+            singleton = new SimpleCustomerServiceImpl();
+        }
+        return singleton;
+    }
+}
